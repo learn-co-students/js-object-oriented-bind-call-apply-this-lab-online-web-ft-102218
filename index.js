370a2f0b601438a67b92efbf7def582ed39969b2
Eric Mayfield <eric@visiblycreative.com>
2019-04-15 16:09:28 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-//Your code here
+function justInvoke(fn) {
+  return fn()
+}
+
+function setThisWithCall(fn, thisValue, arg) {
+  return fn.call(thisValue, arg)
+}
+
+function setThisWithApply(fn, thisValue, args) {
+  return fn.apply(thisValue, arg)
+}
